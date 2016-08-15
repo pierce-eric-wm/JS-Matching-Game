0f9e0c90511297cac529cafae851a0420ca4d4c8
Eric <eric-pierce@west-mec.org>
2016-08-15 15:16:40 -0700

    This fixes the img so it looks better and it is easy to see the stroke on it and it finishes part 2 of the project

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var theRightSide = getElementById("rightSide");
-var theLeftSide = getElementById("leftSide");
+var theRightSide = document.getElementById("rightSide");
+var theLeftSide = document.getElementById("leftSide");
+var numberOfFaces = 5;
+var img="circle.png";
 function generateFaces() {
-    if (i=0)
-    createElement("img.src=circle.png");
+    for (var i=0; i< numberOfFaces; i++ ) {
+        var image = document.createElement("img");
+        image.src = img;
+        image.style.height = "50px";
+        image.style.top = Math.random() * 400 + "px";
+        image.style.left = Math.random() * 400 + "px";
+        theLeftSide.appendChild(image);
+    }
+
 }
+generateFaces();
